cbb41d39b6c77c8168d27211d4236163d5be8fb1
Mariusflores <mada060@student.kristiania.no>
2023-11-01 19:05:15 +0100

    server running with react and a simple hello world

---
+++ client/src/app.jsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export function app() {
+    return <h1>Hello world</h1>;
+}
---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import * as path from "path";
+
+const app = express();
+
+app.use(express.static("../client/dist"))
+
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")){
+        res.sendFile(path.resolve("../client/dist/index.html"))
+    }
+})
+
+const server = app.listen(process.env.port || 3000, () => {
+    console.log("server started at http://localhost:" + server.address().port);
+});
